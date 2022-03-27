b7aa37167681b764cdbc27d4d11998871c2a3027
siksajozef <70346027+siksajozef@users.noreply.github.com>
2022-03-27 13:41:43 +0200

    first 3 parts

---
+++ scripts.js
@@ -0,0 +1,38 @@
+console.log("JavaScript is working!");
+
+let colors = ["orange", "blue", "green", "purple"];
+let post = {
+    title: "My Post",
+    description: "My first post, awesome!",
+    likeCount: 12,
+    comments: ["cool", "nice job!"]
+};
+
+console.log(colors);
+console.log(post);
+
+let age=12;
+if (age > 18) {
+    console.log("You are an adult.");
+} else{
+    console.log("You are NOT an adult."); 
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+colors.forEach(color => {console.log(color);});
+
+let myFunkcion = x => {
+    console.log(x);
+}
+myFunkcion(10);
+
+let greeter = name => {
+    console.log(`Hello, ${name}.`);
+}
+greeter("Adam");
+greeter("Thomas");
+
+console.log("This is the end of our code.");
